38202e18f9a0452eb55fbc7a72f616387af7e3ac
Varun Chandrasekharan <varun@mindhelix.com>
2012-10-23 11:25:12 +0530

    added comment in conatant values

--- src/com/mindhelix/nwplyng/ConstantValues.java
+++ src/com/mindhelix/nwplyng/ConstantValues.java
@@ -5,6 +5,7 @@ public class ConstantValues {
 	public static int TIME_OUT_VALUE = 1000 * 30;
 	public static int TIME_OUT_PLAY = 1000 * 40;
 	
+	//nwplyng secret key used for making digest
 	public static String NWPLYNG_SECRET_KEY = "3485879cdd7b6656151473f5982d8abfc137cbb0"; 
 	
 	public static String NWPLYNG_SHARED_PREFERENCE = "nwplyng_preference1";
